cd0e2f572eb3eaa8430702f60d60e5d29ea554bb
Sakriya777 <sakriya33@gmail.com>
2021-05-03 17:22:58 +0545

    sign up page designed

--- app/src/main/java/com/example/productionprojectfinal/Login.java
+++ app/src/main/java/com/example/productionprojectfinal/Login.java
@@ -1,14 +1,27 @@
 package com.example.productionprojectfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
-
+    ImageView image;
+    TextView title;
+    TextInputEditText email, password;
+    Button frgtpasswd, signin, callSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +32,20 @@ public class Login extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.pewter));
         }
+        title = findViewById(R.id.hellothere);
+        image = findViewById(R.id.logoimagesmall);
+        callSignup = findViewById(R.id.btncallsignup);
+        callSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(title, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this,pairs);
+                startActivity(intent, options.toBundle());
+            }
+        });
     }
 }
